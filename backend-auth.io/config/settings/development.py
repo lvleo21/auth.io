44f802da6abbb324e51fe19f2954c0d7936229e1
Leonardo Veras <verasleonardo210@gmail.com>
2024-09-12 09:08:44 -0300

    ⚰️ remove 'REST_FRAMEWORK' from development file

--- backend-auth.io/config/settings/development.py
+++ backend-auth.io/config/settings/development.py
@@ -3,14 +3,11 @@ from .base import *
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
     }
-}
-
-# Rest Framework
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
 }
